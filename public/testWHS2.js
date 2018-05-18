f9a445641fa6096847aed9d31b794dd08908dce2
viktorb1 <vbabchanik@ucdavis.edu>
2018-05-18 14:34:58 -0700

    Finished the 3 column / 1 column react thing

--- public/testWHS2.js
+++ public/testWHS2.js
@@ -1,12 +1,11 @@
+var photos = [];
+
 // Called when the user pushes the "submit" button
-var photos;
-
-
-
-
 function photoByNumber() {
-
     var nums = document.getElementById("num").value;
+    var message = document.getElementById("note");
+    var reactEl = document.getElementById("react");
+    var results = document.getElementById("results");
 
     if (inputIsValid(nums)) {
         var oReq = new XMLHttpRequest();
@@ -19,41 +18,59 @@ function photoByNumber() {
         function respCallback() {
             photos = JSON.parse(oReq.responseText);
             var query = document.getElementById("query");
-            var display = document.getElementById("photoImg");
-            var errorMessage = document.getElementById("errorMessage");
 
             if (oReq.status == 400) {
-                if(!errorMessage) {
-                    display.src = "";
-                    var errorText = document.createElement("p");
-                    errorText.id = "errorMessage";
-                    errorText.textContent = "Sorry, your request failed!";
-                    query.appendChild(errorText);
-                }
+                
+                message.textContent = "Sorry, your request failed!";
+                message.style.display = "flex";
+                reactEl.style.display = "none"
+                results.style.display = "none";
             } else {
-                if(errorMessage)
-                    errorMessage.remove();
+                message.style.display = "none";
+                reactEl.style.display = "block";
+                results.style.display = "flex";
+
+                var previousQuery = document.querySelectorAll(".searchQueries");
+
+                for(let i = 0; i < previousQuery.length; i++)
+                    results.removeChild(previousQuery[i]);
+
+                for(let i = 0; i < nums.length; i++)
+                {
+                    var searchQuery = document.createElement("p");
+                    searchQuery.textContent = nums[i];
+                    searchQuery.className = "searchQueries";
+                    results.appendChild(searchQuery);
+                }
+
 
                 var urlStart = "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/";
 
-                for(let i = 0; i < photos.length; i++) {
-                    photos[i].src = urlStart + photos[i].filename; // don't need this yet
+                for(let i = 0; i < photos.length; i++) 
+                    photos[i].src = urlStart + photos[i].filename;
+
+                app.setState({ photos: photos });
                     
-                    console.log(photos[i].filename);
-                }
-
-                const reactContainer = document.getElementById("react");
-
-                ReactDOM.render(React.createElement(App),reactContainer);
-
             }
         }
+    } else {
+            // hide react element
+            reactEl.style.display = "none";
+            message.style.display = "flex";
+            message.textContent = "Invalid input, please try again!";
+            results.style.display = "none";
     }
     
 
     // from my server code
     function inputIsValid(url) {
-        nums = url.split(',').map(Number);
+        if (url.length == 0)
+            return false;
+
+        nums = url.split(',');
+
+        for(let i = 0; i < nums.length; i++)
+            nums[i] = parseInt(nums[i]);
         
         if (nums.length == 0)
             return false;
@@ -117,7 +134,7 @@ class ImageTile extends React.Component {
     var _onClick = this.props.onClick;
     var _index = this.props.index;
     var _photo = this.props.photo;
-    var _selected = _photo.selected; // this one is just for readability
+    let _selected = _photo.selected; // this one is just for readability
 
     return (
         React.createElement('div', 
@@ -146,30 +163,52 @@ class ImageTile extends React.Component {
 } // class
 
 
-
+var updatePhotos = {};
 // The react component for the whole image gallery
 // Most of the code for this is in the included library
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { photos: photos };
-    this.selectTile = this.selectTile.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { photos: photos };
+        this.selectTile = this.selectTile.bind(this);
+    }
 
-  selectTile(event, obj) {
-    console.log("in onclick!", obj);
-    let photos = this.state.photos;
-    photos[obj.index].selected = !photos[obj.index].selected;
-    this.setState({ photos: photos });
-  }
+    selectTile(event, obj) {
+        let photos = this.state.photos;
+        photos[obj.index].selected = !photos[obj.index].selected;
+        this.setState({ photos: photos });
+    }
 
-  render() {
-    return (
-       React.createElement( Gallery, {photos: photos, 
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    // make sure to remove the listener
+    // when the component is not mounted anymore
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange() {
+        app.setState({ width: window.innerWidth });
+    };
+
+    render() {
+        const { width } = this.state;
+        const isMobile = width <= 550;
+
+        if(isMobile)
+            return (React.createElement( Gallery, {photos: this.state.photos, 
            onClick: this.selectTile, 
-           ImageComponent: ImageTile} )
-        );
+           ImageComponent: ImageTile, columns: 1} ));
+        else
+           return (React.createElement( Gallery, {photos: this.state.photos, 
+           onClick: this.selectTile, 
+           ImageComponent: ImageTile, columns: 2} ));
   }
 
 }
+
+const reactContainer = document.getElementById("react");
+var app = ReactDOM.render(React.createElement(App),reactContainer);
