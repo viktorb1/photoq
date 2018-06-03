90309672dbf8f70b279074a2e712c3f81fb7f3be
Viktor Babchanik <vbabchanik@ucdavis.edu>
2018-06-02 23:00:47 -0700

    Sorta done with part 1

--- loadNames.js
+++ loadNames.js
@@ -5,6 +5,25 @@ var sizeOf = require('image-size');
 var url = require('url');
 var http = require('http');
 
+// Node module for working with a request to an API or other fellow-server
+var APIrequest = require('request');
+
+requests = [];
+// An object containing the data the CCV API wants
+// Will get stringified and put into the body of an HTTP request, below
+
+imgprops = {
+    location: [],
+    tags: [],
+    height: [],
+    width: [],
+    fileName: []
+}
+
+// URL containing the API key 
+// You'll have to fill in the one you got from Google
+apiurl = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBF-LfsGHI1RFHbHXIzgMr_rx_vnPXOZhE';
+
 http.globalAgent.maxSockets = 1;
 
 var dbFileName = "PhotoQ.db";
@@ -13,42 +32,128 @@ var numURLs;
 var db = new sqlite3.Database(dbFileName);
 var count = 0;
 
+
 function loadDatabase() {
     var file = fs.readFileSync('photoList.json', 'utf8');
     var photoURLs = JSON.parse(file).photoURLs;
     numURLs = photoURLs.length;
 
     for (let i = 0; i < numURLs; i++) {
+        requests.push({ "image": { "source": {"imageUri": photoURLs[i]} },
+        "features": [{ "type": "LABEL_DETECTION", "maxResults": 6}, { "type": "LANDMARK_DETECTION"} ]});
 
         var options = url.parse(photoURLs[i]);
 
-        http.get(options, function (response) {
-            response.on('data', getChunks).on('end', getDimensions);
-        });
+        http.get(options, 
+            function (response) {
+                response.on('data', getChunks).on('end', getDimensions);
+            }
+        );
 
         let chunks = [];
 
         function getChunks(chunk) {
-                chunks.push(chunk);
+            chunks.push(chunk);
         }
 
         function getDimensions() {
             var buffer = Buffer.concat(chunks);
             var dimensions = sizeOf(buffer);
 
-            var height = dimensions.height;
-            var width = dimensions.width;
-            var fileName = photoURLs[i].split('/').pop();
+            imgprops.height.push(dimensions.height);
+            imgprops.width.push(dimensions.width)
+            imgprops.fileName.push(photoURLs[i].split('/').pop());
+            console.log("Getting dimensions for " + i);
 
-            insertIntoDB(fileName, width, height, i);
+            if (imgprops.height.length == numURLs) {
+                console.log("Done getting image dimensions!");
+                getTags();
+            }
         }
     }
 }
 
 
-function insertIntoDB(fileName, width, height, i) {
-    var sql = "INSERT INTO photoTags VALUES(" + i + ", '" + fileName + "', " + width + ", " + height + ", '', '')";
-    db.run(sql, doneInserting);
+function getTags() {
+    let timeToWait = 0;
+
+    for (let i = 0; i < numURLs; i++) {
+
+        let APIrequestObject = { "requests": []}
+        APIrequestObject.requests = requests.slice(i, i+1);
+        timeToWait += 500;
+        setTimeout(annotateImage, timeToWait);
+
+        function annotateImage() {
+            // The code that makes a request to the API
+            // Uses the Node request module, which packs up and sends off 
+            // an HTTP message containing the request to the API server
+            APIrequest(
+                { // HTTP header stuff
+                    url: apiurl,
+                    method: "POST",
+                    headers: {"content-type": "application/json"},
+                    // will turn the given object into JSON
+                    json: APIrequestObject
+                },
+                // callback function for API request
+                APIcallback
+            );
+
+            // callback function, called when data is received from API
+            function APIcallback(err, APIresponse, body) {
+                if ((err) || (APIresponse.statusCode != 200)) {
+                    console.log("Got API error");
+                    console.log(body);
+                } else {
+                    for(let i = 0; i < body.responses.length; i++) {
+                        let next = body.responses[i];
+
+                        if (next.labelAnnotations) {
+                            let imgtg = [];
+                            
+                            for (let i = 0; i < next.labelAnnotations.length; i++)
+                                imgtg.push(next.labelAnnotations[i].description);
+
+                            console.log(imgtg.join(','));
+                            imgprops.tags.push(imgtg.join(','));
+                        } else {
+                            console.log("This shouldn't fail");
+                            imgprops.tags.push('');
+                        }
+
+                        if (next.landmarkAnnotations && next.landmarkAnnotations[0].description)
+                            imgprops.location.push(next.landmarkAnnotations[0].description);
+                        else
+                            imgprops.location.push('');
+
+
+                        if (next.error) {
+                            console.log("an error occurred");
+                            console.log(next.error);
+                        }
+                    }
+
+                    let max =  i + body.responses.length;
+
+                    console.log("Now inserting: " + i + " - " + (max-1));
+                    insertIntoDB(i, max);
+                }
+            } // end callback function
+        }
+    }
+}
+
+
+function insertIntoDB(i, max) {
+    for (i; i < max; i++) {
+        console.log("LOCATION: " + imgprops.location[i]);
+        console.log("TAGS: " + imgprops.tags[i]);
+
+        let sql = "INSERT INTO photoTags VALUES(?, ?, ?, ?, ?, ?)";
+        
+        db.run(sql, [i, imgprops.fileName[i],  imgprops.height[i], imgprops.location[i], imgprops.tags[i]], doneInserting);
+    }
 }
 
 
@@ -58,8 +163,7 @@ function doneInserting(error) {
     else
         count++;
 
-    if (count == numURLs)
-    {
+    if (count == numURLs) {
         console.log("DONE!");
         db.close();
     }
---
+++ test.js
@@ -0,0 +1,342 @@
+var lol = [
+        {
+                "landmarkAnnotations": [
+                        {
+                                "mid": "/m/06v230",
+                                "description": "Bassae, Temple of Apollo Epicurius",
+                                "score": 0.98351187,
+                                "boundingPoly": {
+                                        "vertices": [
+                                                {
+                                                        "x": 23,
+                                                        "y": 26
+                                                },
+                                                {
+                                                        "x": 1573,
+                                                        "y": 26
+                                                },
+                                                {
+                                                        "x": 1573,
+                                                        "y": 1179
+                                                },
+                                                {
+                                                        "x": 23,
+                                                        "y": 1179
+                                                }
+                                        ]
+                                },
+                                "locations": [
+                                        {
+                                                "latLng": {
+                                                        "latitude": 37.429444,
+                                                        "longitude": 21.900306
+                                                }
+                                        }
+                                ]
+                        }
+                ],
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/0n68_",
+                                "description": "structure",
+                                "score": 0.8703026,
+                                "topicality": 0.8703026
+                        },
+                        {
+                                "mid": "/m/01_m7",
+                                "description": "column",
+                                "score": 0.86600816,
+                                "topicality": 0.86600816
+                        },
+                        {
+                                "mid": "/m/0djmp",
+                                "description": "ancient history",
+                                "score": 0.74574393,
+                                "topicality": 0.74574393
+                        },
+                        {
+                                "mid": "/m/0cgh4",
+                                "description": "building",
+                                "score": 0.7445654,
+                                "topicality": 0.7445654
+                        },
+                        {
+                                "mid": "/m/07yr8h",
+                                "description": "historic site",
+                                "score": 0.7300496,
+                                "topicality": 0.7300496
+                        },
+                        {
+                                "mid": "/m/0dvg9",
+                                "description": "ancient roman architecture",
+                                "score": 0.5755491,
+                                "topicality": 0.5755491
+                        }
+                ]
+        },
+        {
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/07yr8h",
+                                "description": "historic site",
+                                "score": 0.9260156,
+                                "topicality": 0.9260156
+                        },
+                        {
+                                "mid": "/m/0djmp",
+                                "description": "ancient history",
+                                "score": 0.8230526,
+                                "topicality": 0.8230526
+                        },
+                        {
+                                "mid": "/m/012ww9",
+                                "description": "archaeological site",
+                                "score": 0.76394045,
+                                "topicality": 0.76394045
+                        },
+                        {
+                                "mid": "/m/0cx45",
+                                "description": "temple",
+                                "score": 0.72777635,
+                                "topicality": 0.72777635
+                        },
+                        {
+                                "mid": "/m/08g_yr",
+                                "description": "temple",
+                                "score": 0.72506505,
+                                "topicality": 0.72506505
+                        },
+                        {
+                                "mid": "/m/02nvjx",
+                                "description": "place of worship",
+                                "score": 0.7066198,
+                                "topicality": 0.7066198
+                        }
+                ]
+        },
+        {
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/0cgh4",
+                                "description": "building",
+                                "score": 0.9232834,
+                                "topicality": 0.9232834
+                        },
+                        {
+                                "mid": "/m/0dvh9",
+                                "description": "classical architecture",
+                                "score": 0.9115241,
+                                "topicality": 0.9115241
+                        },
+                        {
+                                "mid": "/m/05_5t0l",
+                                "description": "landmark",
+                                "score": 0.90727246,
+                                "topicality": 0.90727246
+                        },
+                        {
+                                "mid": "/m/05wrt",
+                                "description": "property",
+                                "score": 0.8867928,
+                                "topicality": 0.8867928
+                        },
+                        {
+                                "mid": "/m/0dx1j",
+                                "description": "town",
+                                "score": 0.83478874,
+                                "topicality": 0.83478874
+                        },
+                        {
+                                "mid": "/m/02pvbb",
+                                "description": "mansion",
+                                "score": 0.8264347,
+                                "topicality": 0.8264347
+                        }
+                ]
+        },
+        {
+                "landmarkAnnotations": [
+                        {
+                                "mid": "/m/06k7dl",
+                                "description": "Town Hall Square",
+                                "score": 0.8538471,
+                                "boundingPoly": {
+                                        "vertices": [
+                                                {
+                                                        "x": 103,
+                                                        "y": 499
+                                                },
+                                                {
+                                                        "x": 1624,
+                                                        "y": 499
+                                                },
+                                                {
+                                                        "x": 1624,
+                                                        "y": 827
+                                                },
+                                                {
+                                                        "x": 103,
+                                                        "y": 827
+                                                }
+                                        ]
+                                },
+                                "locations": [
+                                        {
+                                                "latLng": {
+                                                        "latitude": 49.29261600517235,
+                                                        "longitude": 21.276058
+                                                }
+                                        }
+                                ]
+                        }
+                ],
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/0dx1j",
+                                "description": "town",
+                                "score": 0.9574561,
+                                "topicality": 0.9574561
+                        },
+                        {
+                                "mid": "/m/01wkk9",
+                                "description": "town square",
+                                "score": 0.91776806,
+                                "topicality": 0.91776806
+                        },
+                        {
+                                "mid": "/m/05_5t0l",
+                                "description": "landmark",
+                                "score": 0.8929299,
+                                "topicality": 0.8929299
+                        },
+                        {
+                                "mid": "/m/01bqvp",
+                                "description": "sky",
+                                "score": 0.8858255,
+                                "topicality": 0.8858255
+                        },
+                        {
+                                "mid": "/m/01n32",
+                                "description": "city",
+                                "score": 0.8847262,
+                                "topicality": 0.8847262
+                        },
+                        {
+                                "mid": "/m/01ht1r",
+                                "description": "medieval architecture",
+                                "score": 0.8015485,
+                                "topicality": 0.8015485
+                        }
+                ]
+        },
+        {
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/0dx1j",
+                                "description": "town",
+                                "score": 0.93848157,
+                                "topicality": 0.93848157
+                        },
+                        {
+                                "mid": "/m/0180xr",
+                                "description": "neighbourhood",
+                                "score": 0.7828259,
+                                "topicality": 0.7828259
+                        },
+                        {
+                                "mid": "/m/01c8br",
+                                "description": "street",
+                                "score": 0.7646846,
+                                "topicality": 0.7646846
+                        },
+                        {
+                                "mid": "/m/023907r",
+                                "description": "real estate",
+                                "score": 0.6648337,
+                                "topicality": 0.6648337
+                        },
+                        {
+                                "mid": "/m/06gfj",
+                                "description": "road",
+                                "score": 0.6548746,
+                                "topicality": 0.6548746
+                        },
+                        {
+                                "mid": "/m/01n32",
+                                "description": "city",
+                                "score": 0.61126786,
+                                "topicality": 0.61126786
+                        }
+                ]
+        },
+        {
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/0f0q9",
+                                "description": "village",
+                                "score": 0.72442335,
+                                "topicality": 0.72442335
+                        },
+                        {
+                                "mid": "/m/08yp1s",
+                                "description": "hut",
+                                "score": 0.6760956,
+                                "topicality": 0.6760956
+                        },
+                        {
+                                "mid": "/m/07j7r",
+                                "description": "tree",
+                                "score": 0.6658361,
+                                "topicality": 0.6658361
+                        },
+                        {
+                                "mid": "/m/05s2s",
+                                "description": "plant",
+                                "score": 0.6527948,
+                                "topicality": 0.6527948
+                        }
+                ]
+        },
+        {
+                "labelAnnotations": [
+                        {
+                                "mid": "/m/05wrt",
+                                "description": "property",
+                                "score": 0.92199767,
+                                "topicality": 0.92199767
+                        },
+                        {
+                                "mid": "/m/03jm5",
+                                "description": "house",
+                                "score": 0.8865026,
+                                "topicality": 0.8865026
+                        },
+                        {
+                                "mid": "/m/0dx1j",
+                                "description": "town",
+                                "score": 0.88193667,
+                                "topicality": 0.88193667
+                        },
+                        {
+                                "mid": "/m/0cgh4",
+                                "description": "building",
+                                "score": 0.8698165,
+                                "topicality": 0.8698165
+                        },
+                        {
+                                "mid": "/m/01bqvp",
+                                "description": "sky",
+                                "score": 0.8515287,
+                                "topicality": 0.8515287
+                        },
+                        {
+                                "mid": "/m/09qqq",
+                                "description": "wall",
+                                "score": 0.803996,
+                                "topicality": 0.803996
+                        }
+                ]
+        }
+]
+
+console.log(lol[0].landmarkAnnotations[0].description);
